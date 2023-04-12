c6a44ee34bce79c3aeb040336efd1cc623581fc8
lorenzoGigliMW <lorenzo.gigli@markertwall.com>
2023-04-12 10:32:20 +0200

    serverAgg

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const bodyParser = require('body-parser');
 var cors = require('cors')
 
 var todos = [
-    { id: "todo-0", name: "Eat", completed: true },
-    { id: "todo-1", name: "Sleep", completed: false },
-    { id: "todo-2", name: "Repeat", completed: false }
+    // { id: "todo-0", name: "Eat", completed: true },
+    // { id: "todo-1", name: "Sleep", completed: false },
+    // { id: "todo-2", name: "Repeat", completed: false }
 ]
 
 const app = express();
